6929e25648039baa1812cdaff988d04067aa0a14
aqude <aqu.de@yandex.ru>
2023-06-09 00:55:50 +0300

    add views-screens

---
+++ vue/src/views/Login.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "Login"
+})
+</script>
+
+<template>
+    <h1 class="text-2xl text-blue">Login</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/src/views/Register.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: "Register"
+})
+</script>
+
+<template>
+    <h1>Register</h1>
+</template>
+
+<style scoped>
+
+</style>
