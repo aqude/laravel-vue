15c06c8b190223484148d426ed0d914d028140f2
aqude <aqu.de@yandex.ru>
2023-06-09 18:42:09 +0300

    edit router

--- vue/src/views/DataList.vue
+++ vue/src/views/DataList.vue
@@ -24,7 +24,7 @@ const navigation = [
 const userNavigation = [
     { name: 'Ваш профиль', path: 'profile' },
     { name: 'Настройка', path: 'settings' },
-    { name: 'Выйти', path: 'login-out' },
+    { name: 'Выйти', path: 'login'}, // login-out надо реализовать
 ]
 </script>
 
